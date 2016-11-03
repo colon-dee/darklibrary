afbd7b76c08e8ee196fa591ae35753b0a91e08b8
D <colondee@protonmail.com>
2016-11-03 21:16:43 -0200

    Change index name and create a new table

--- darklibrary.sql
+++ darklibrary.sql
@@ -9,21 +9,36 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `darklibrary` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `darklibrary`;
 
-DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Invitations` (
+  `idInvitation` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `code` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `Users` (
-  `codUser` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
   `name` varchar(32) NOT NULL,
   `password` varchar(255) NOT NULL,
   `approved` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+ALTER TABLE `Invitations`
+  ADD PRIMARY KEY (`idInvitation`),
+  ADD KEY `idUser` (`idUser`);
+
 ALTER TABLE `Users`
-  ADD PRIMARY KEY (`codUser`);
+  ADD PRIMARY KEY (`idUser`);
 
 
+ALTER TABLE `Invitations`
+  MODIFY `idInvitation` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `Users`
-  MODIFY `codUser` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `Invitations`
+  ADD CONSTRAINT `fk_idUser_Users_Invitations` FOREIGN KEY (`idUser`) REFERENCES `Users` (`idUser`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
